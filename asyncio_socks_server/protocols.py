fc2ec646f6217ef3314ff612c4c1961306ae694c
Pavel Piatruk <erik.zbik@gmail.com>
2024-04-03 18:00:12 +0300

    px

--- asyncio_socks_server/protocols.py
+++ asyncio_socks_server/protocols.py
@@ -31,7 +31,17 @@ def query(resolver, name, query_type):
         print(e)
 
 def acl(config, DST_ADDR):
-    if config.DENY_RAW_IP_ADDRESSES==True and re.se
+    def is_ipv4(address):
+        ipv4_pattern = r'^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+        return re.match(ipv4_pattern, address) is not None
+
+    def is_ipv6(address):
+        ipv6_pattern = r'^(([0-9a-fA-F]{1,4}):){7}([0-9a-fA-F]{1,4})$'
+        return re.match(ipv6_pattern, address) is not None
+
+    if config.DENY_RAW_IP_ADDRESSES==True and ( is_ipv4(DST_ADDR) or is_ipv6(DST_ADDR) ):
+        return -1
+
     for banned_dst in config.BANNED_DST :
         if re.search(rf'\.?{banned_dst}$' , DST_ADDR):
             return -1
@@ -458,6 +468,9 @@ class LocalUDP(asyncio.DatagramProtocol):
             config.ACCESS_LOG and access_logger.debug(
                 f'[UDP] {HNAME} resolved to {DST_ADDR}'
             )
+
+        if acl(config, HNAME) == -1:
+            raise NoAtypAllowed(f"ACL: Not allowed to call IP {DST_ADDR}")
 
         return RSV, FRAG, ATYP, DST_ADDR, DST_PORT, length
 
