3503049a72ae50e02bb6232c7876bf796a3160b7
PavelPiatruk <erik.zbik@gmail.com>
2024-04-14 13:43:54 +0300

    px

---
+++ config.js
@@ -0,0 +1,12 @@
+{
+    "LISTEN_HOST": "0.0.0.0",
+    "LISTEN_PORT": 5001,
+    "AUTH_METHOD": 2,
+    "ACCESS_LOG": true,
+    "STRICT": false,
+    "DEBUG": true,
+    "USERS": {
+        "alice": "cool"
+    },
+    "RESOLVER": "1.1.1.1"
+}
